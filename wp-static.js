56f293f086ce09a30cc2f24a0d0543e4831e035f
adlinkfly <46087611+Farouk-dz@users.noreply.github.com>
2021-11-10 21:43:47 +0100

    Update and rename wpstatic.js to wp-static.js

--- wp-static.js
+++ wp-static.js
@@ -1,7 +1,5 @@
-var bod = document.getElementsByTagName('body');
 if(!document.getElementById('copy')){
-bod.innerHTML = "Database Error!"
+document.getElementsByTagName('body').innerHTML = "<p><strong>Database Error!</strong></p>"
 }if(document.getElementById('copy')){
 document.getElementById("copy").innerHTML = '<p class="m-0 text-center text-white">All Logos & Trademark Belongs To Their Respective Owners.</p><br><p class="m-0 text-center text-white">Made with <span class="red">♥</span> by <a class="text-white" href="https://fb.me/btc.31">FaroukDev</a></p>';
-
 }

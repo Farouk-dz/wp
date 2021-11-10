dd7a7e84bd12f118fde8f2a50ba6adc4c1379c07
adlinkfly <46087611+Farouk-dz@users.noreply.github.com>
2021-11-10 19:55:07 +0100

    Create bundle.js

---
+++ bundle.js
@@ -0,0 +1,1 @@
+    document.getElementById("copy").innerHTML = '<p class="m-0 text-center text-white">All Logos & Trademark Belongs To Their Respective Owners.</p><br><p class="m-0 text-center text-white">Made with <span class="red">♥</span> by <a class="text-white" href="https://fb.me/btc.31">FaroukDev</a></p>';

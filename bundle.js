c0e068da23625a3b3c8a4d860a44a86a2f446370
adlinkfly <46087611+Farouk-dz@users.noreply.github.com>
2021-11-10 21:01:24 +0100

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,1 +1,7 @@
+var bod = document.getElementByTag('body');
+if(!document.getElementById('copy')){
+bod.innerHTML = "Database Error!"
+}else{
 document.getElementById("copy").innerHTML = '<p class="m-0 text-center text-white">All Logos & Trademark Belongs To Their Respective Owners.</p><br><p class="m-0 text-center text-white">Made with <span class="red">♥</span> by <a class="text-white" href="https://fb.me/btc.31">FaroukDev</a></p>';
+
+}

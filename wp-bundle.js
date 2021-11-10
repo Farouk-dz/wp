5d4947658402faff42f5a9b7eeb73c262416dd4c
adlinkfly <46087611+Farouk-dz@users.noreply.github.com>
2021-11-10 23:14:10 +0100

    Update and rename wp-bundle.js to jquery-bundle.js

--- wp-bundle.js
+++
@@ -1,7 +0,0 @@
-window.onload(function(){
-if(!document.getElementById('copy')){
-document.getElementsByTagName("body").innerHTML = "<p><strong>Database Error!</strong></p>"
-}else{
-document.getElementById("footer").innerHTML = '<p class="m-0 text-center text-white">All Logos & Trademark Belongs To Their Respective Owners.</p><br><p class="m-0 text-center text-white">Made with <span class="red">♥</span> by <a class="text-white" href="https://fb.me/btc.31">FaroukDev</a></p>';
-}
-});
